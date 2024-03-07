1b2d5092007f2c9f501ec9d9ac52165773ec0914
RagTagQK <162122544+RagTagQK@users.noreply.github.com>
2024-03-07 20:14:33 +0500

    Delete Diplom directory

--- Diplom/src/main/java/com/example/diplom/repo/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.diplom.repo;
-
-import com.example.diplom.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
